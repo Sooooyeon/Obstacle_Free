3b6c16087180f6489bba5b5e0290d8581e4ece01
Sooooyeon <tn7278@naver.com>
2024-04-05 17:24:48 +0900

    feat: 관광정보 출력, 상세페이지 라우팅

---
+++ getData.js
@@ -0,0 +1,50 @@
+import { config } from "./api.js";
+
+const getTouristAreas = () => {
+  console.log('config');
+  $.ajax({
+    method: "GET",
+    url:`https://apis.data.go.kr/B551011/KorWithService1/areaBasedList1?ServiceKey=${config.apikey}&numOfRows=6&pageNo=2&MobileOS=ETC&MobileApp=TestApp&contentTypeId=12&_type=json`
+  })
+  .done((data)=>{
+    const tourlistareas = data.response.body.items;
+    console.log(tourlistareas);
+    console.log(tourlistareas.item[0]);
+    console.log(tourlistareas.item[0].title);
+    console.log(tourlistareas.item[0].addr1);
+    if(tourlistareas !== undefined){
+      console.log(typeof(tourlistareas));
+      $.map(tourlistareas.item, function(item){
+        console.log(item);
+        if(item.firstimage !==""){
+          return $("#tourAreas").append(
+          `<li id=${item.contentid} class="area">
+            <img src=${item.firstimage} alt=${item.title}>
+            <p id="tourAreaTitle">${item.title}</p>
+            <p id="tourAreaAddress">주소 : ${item.addr1}</p>
+          </li>`)
+        } else {
+          return $("#tourAreas").append(
+            `<li id=${item.contentid} class="area">
+              <img src="./img/tuorareadefault.png" alt=${item.title}>
+              <p id="tourAreaTitle">${item.title}</p>
+              <p id="tourAreaAddress">주소 : ${item.addr1}</p>
+            </li>`)
+        }
+        
+        
+      })
+    }
+  })
+}
+
+getTouristAreas();
+
+const moveDetail = (url, id) => {
+  window.location.href = url + '?contentid=' + id;
+}
+
+$("#tourAreas").on("click", ".area", function(){
+  console.log(this.id);
+  moveDetail('areaDetail.html',this.id);
+});
