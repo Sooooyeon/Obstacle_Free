579393ba960dbfaf1de25ada8668f4d8a0d43320
Sooooyeon <tn7278@naver.com>
2024-04-15 17:51:19 +0900

    feat: 위치기반 관광지 목록 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { config } from "../api.js";
 import { recommendPlace } from "./recommendPlace.js";
 
 $.map(recommendPlace, function(item){
@@ -66,3 +67,48 @@ $("#menu").on("click", ".category", function(){
   console.log(url);
   if(url !== "") moveCategory(url, contentTypeId);
 });
+
+
+navigator.geolocation.getCurrentPosition(geoSuccess, getError);
+
+function geoSuccess(position) {
+    
+  const lat = position.coords.latitude; // 위도
+  const lng = position.coords.longitude; // 경도
+    if(lat && lng){
+      $.ajax({
+        method: "GET",
+        url:`https://apis.data.go.kr/B551011/KorWithService1/locationBasedList1?ServiceKey=${config.apikey}&numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=TestApp&mapX=${lng}&mapY=${lat}&radius=1000&_type=json`
+      })
+      .done((data)=>{
+        const nearbyAreas = data.response.body.items;
+        console.log(nearbyAreas);
+        $("#nearbyAreas").empty();
+        if(nearbyAreas !== undefined){
+          $.map(nearbyAreas.item, function(item){
+            if(item.firstimage !==""){
+              return $("#nearbyAreas").append(
+              `<li id=${item.contentid} class="area" title="${item.title}" data-image="${item.firstimage}" data-addr1="${item.addr1}">
+                <img src=${item.firstimage} alt="${item.title}">
+                <p id="tourAreaTitle">${item.title}</p>
+                <p id="tourAreaAddress">${item.addr1}</p>
+              </li>`)
+            } else {
+              return $("#nearbyAreas").append(
+                `<li id=${item.contentid} class="area" title="${item.title}" data-image="${item.firstimage}" data-addr1="${item.addr1}">
+                  <img src="../img/tuorareadefault.png" alt="${item.title}">
+                  <p id="tourAreaTitle">${item.title}</p>
+                  <p id="tourAreaAddress">${item.addr1}</p>
+                </li>`)
+            }
+          })
+        }
+      })
+    }
+}
+
+
+function getError() {
+  alert('Geolocation Error');
+}
+
